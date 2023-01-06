9e0c680d56e24ebe2e48a8dbefaae63977520d7d
wujinhua <1938054289@qq.com>
2023-01-06 16:53:26 +0800

    feat: 物体阴影

--- src/main/main.js
+++ src/main/main.js
@@ -1,16 +1,8 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
 
 // 创建场景
 const scene = new THREE.Scene();
-
-const rgbeLoader = new RGBELoader();
-rgbeLoader.loadAsync('textures/hdr/1.hdr').then((texture) => {
-  texture.mapping = THREE.EquirectangularReflectionMapping;
-  scene.background = texture;
-  scene.environment = texture;
-});
 
 // 创建相机 函数总共有四个参数，分别是fov，aspect，near，far 。fov表示摄像机视锥体垂直视野角度，最小值为0，最大值为180，默认值为50，实际项目中一般都定义45，因为45最接近人正常睁眼角度；aspect表示摄像机视锥体长宽比，默认长宽比为1，即表示看到的是正方形，实际项目中使用的是屏幕的宽高比；near表示摄像机视锥体近端面，这个值默认为0.1，实际项目中都会设置为1；far表示摄像机视锥体远端面，默认为2000，这个值可以是无限的，说的简单点就是我们视觉所能看到的最远距离。
 /**
@@ -44,36 +36,25 @@ const event = {
   },
 };
 
-// 加载管理器
-const loadManger = new THREE.LoadingManager(
-  event.onLoad,
-  event.onProgress,
-  event.onError
-);
-
-// 设置cube纹理加载器
-const cubeTextureLoader = new THREE.CubeTextureLoader(loadManger);
-const envMapTexture = cubeTextureLoader.load([
-  './textures/env/1.jpg',
-  './textures/env/2.jpg',
-  './textures/env/3.jpg',
-  './textures/env/4.jpg',
-  './textures/env/5.jpg',
-  './textures/env/6.jpg',
-]);
 const sphereGeometry = new THREE.SphereGeometry(1, 20, 20);
-const material = new THREE.MeshStandardMaterial({
-  metalness: 0.7,
-  roughness: 0.1,
-  // envMap: envMapTexture,
-});
+const material = new THREE.MeshStandardMaterial();
 
 const sphere = new THREE.Mesh(sphereGeometry, material);
+// 投射阴影
+sphere.castShadow = true;
+
 scene.add(sphere);
-// 添加场景背景
-scene.background = envMapTexture;
-// 给场景所有物体添加环境贴图
-scene.environment = envMapTexture;
+
+// 创建平面
+const planeGeometry = new THREE.PlaneGeometry(10, 10);
+const plane = new THREE.Mesh(planeGeometry, material);
+plane.position.set(0, -1, 0);
+plane.rotation.x = -Math.PI / 2;
+
+// 接收阴影
+plane.receiveShadow = true;
+
+scene.add(plane);
 
 // 灯光
 // 环境光
@@ -82,10 +63,16 @@ scene.environment = envMapTexture;
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.set(10, 10, 10);
+
+directionalLight.castShadow = true;
+
 scene.add(directionalLight);
 
 // 初始化渲染器
 const renderer = new THREE.WebGLRenderer();
+
+// 开启环境中的阴影贴图
+renderer.shadowMap.enabled = true;
 
 // 设置渲染尺寸大小
 renderer.setSize(window.innerWidth, window.innerHeight);
