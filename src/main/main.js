3f1af293b682626c51a4e6b97ff91f826fcd1bc1
wujinhua <1938054289@qq.com>
2023-01-11 16:05:51 +0800

    feat: 滚动特效

--- src/main/main.js
+++ src/main/main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import * as dat from 'dat.gui';
+import gsap from 'gsap';
 
 const gui = new dat.GUI();
 
@@ -16,35 +17,35 @@ const scene = new THREE.Scene();
  */
 
 const camera = new THREE.PerspectiveCamera(
-  45,
+  85,
   window.innerWidth / window.innerHeight,
-  1,
-  40
+  0.1,
+  300
 );
 
 // 设置相机位置
-camera.position.set(0, 0, 10); // x y z 轴
+camera.position.set(0, 0, 18); // x y z 轴
 scene.add(camera);
 
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
+const cubeGeometry = new THREE.BoxGeometry(2, 2, 2);
+const cubeMaterial = new THREE.MeshBasicMaterial({
   wireframe: true,
 });
 const redMaterial = new THREE.MeshBasicMaterial({ color: '#ff0000' });
 
 let cubeArr = [];
-
-for (let x = -5; x < 5; x++) {
-  for (let y = -5; y < 5; y++) {
-    for (let z = -5; z < 5; z++) {
-      const cube = new THREE.Mesh(cubeGeometry, material);
-      cube.position.set(x, y, z);
-      scene.add(cube);
+const cubeGround = new THREE.Group();
+for (let x = 0; x < 5; x++) {
+  for (let y = 0; y < 5; y++) {
+    for (let z = 0; z < 5; z++) {
+      const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+      cube.position.set(x * 2 - 4, y * 2 - 4, z * 2 - 4);
+      cubeGround.add(cube);
       cubeArr.push(cube);
     }
   }
 }
-
+scene.add(cubeGround);
 // 创建投射光线对象
 const raycaster = new THREE.Raycaster();
 
@@ -64,13 +65,92 @@ window.addEventListener('click', (event) => {
   // });
 });
 
+// 三角形
+const sjxGrounp = new THREE.Group();
+for (let x = 0; x < 50; x++) {
+  const geometry = new THREE.BufferGeometry();
+  const positionArr = new Float32Array(9);
+  for (let y = 0; y < 9; y++) {
+    if (y % 3 == 1) {
+      positionArr[y] = Math.random() * 10 - 5;
+    } else {
+      positionArr[y] = Math.random() * 10 - 5;
+    }
+  }
+
+  geometry.setAttribute('position', new THREE.BufferAttribute(positionArr, 3));
+  // 创建材质
+  const color = new THREE.Color(Math.random(), Math.random(), Math.random());
+  const material = new THREE.MeshBasicMaterial({
+    color,
+    transparent: true,
+    opacity: 0.5,
+    side: THREE.DoubleSide,
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+  sjxGrounp.add(mesh);
+}
+sjxGrounp.position.set(0, -30, 0);
+scene.add(sjxGrounp);
+
+// 弹跳小球
+const ballGrounp = new THREE.Group();
+
+const sphereGeometry = new THREE.SphereGeometry(1, 20, 20);
+const material = new THREE.MeshStandardMaterial();
+
+const sphere = new THREE.Mesh(sphereGeometry, material);
+// 投射阴影
+sphere.castShadow = true;
+
+ballGrounp.add(sphere);
+
+// 创建平面
+const planeGeometry = new THREE.PlaneGeometry(20, 20);
+const plane = new THREE.Mesh(planeGeometry, material);
+plane.position.set(0, -1, 0);
+plane.rotation.x = -Math.PI / 2;
+
+// 接收阴影
+plane.receiveShadow = true;
+
+ballGrounp.add(plane);
+
+const smallBall = new THREE.Mesh(
+  new THREE.SphereGeometry(0.1, 20, 20),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+smallBall.position.set(2, 2, 2);
+
+// 灯光
+// 环境光
+// const light = new THREE.AmbientLight(0xffffff, 0.5);
+// scene.add(light);
+
+const pointLight = new THREE.PointLight(0xff0000, 20);
+pointLight.position.set(2, 2, 2);
+
+pointLight.castShadow = true;
+
+// 阴影贴图模糊度
+pointLight.shadow.radius = 20;
+// 阴影贴图分辨率
+pointLight.shadow.mapSize.set(512, 512);
+
+smallBall.add(pointLight);
+ballGrounp.add(smallBall);
+
+ballGrounp.position.set(0, -60, 0);
+scene.add(ballGrounp);
+
 // 初始化渲染器
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ alpha: true });
 
 renderer.physicallyCorrectLights = true;
 
 // 开启环境中的阴影贴图
 renderer.shadowMap.enabled = true;
+renderer.physicallyCorrectLights = true;
 
 // 设置渲染尺寸大小
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -82,9 +162,9 @@ document.body.appendChild(renderer.domElement);
 // renderer.render(scene, camera);
 
 // 创建轨道控制器
-const controls = new OrbitControls(camera, renderer.domElement);
-// 增加阻尼，拖动具有惯性效果
-controls.enableDamping = true;
+// const controls = new OrbitControls(camera, renderer.domElement);
+// // 增加阻尼，拖动具有惯性效果
+// controls.enableDamping = true;
 
 // 添加坐标轴
 const axesHelper = new THREE.AxesHelper(5);
@@ -119,12 +199,97 @@ window.addEventListener('resize', () => {
   renderer.setPixelRatio(window.devicePixelRatio);
 });
 
+gsap.to(cubeGround.position, {
+  z: `+=${Math.PI}`,
+  x: `+=${Math.PI}`,
+  duration: 6,
+  ease: 'power2.inOut',
+  repeat: -1,
+  yoyo: true,
+});
+
+gsap.to(sjxGrounp.position, {
+  z: `+=${Math.PI}`,
+  x: `-=${Math.PI}`,
+  duration: 6,
+  ease: 'power2.inOut',
+  repeat: -1,
+  yoyo: true,
+});
+
+gsap.to(smallBall.position, {
+  x: -3,
+  duration: 6,
+  ease: 'power2.inOut',
+  repeat: -1,
+  yoyo: true,
+});
+gsap.to(smallBall.position, {
+  y: 0,
+  duration: 0.5,
+  ease: 'power2.inOut',
+  repeat: -1,
+  yoyo: true,
+});
+
+window.addEventListener('mouseover', (event) => {
+  mouse.x = event.clientX / window.innerWidth - 0.5;
+  mouse.y = event.clientY / window.innerHeight - 0.5;
+});
+
 const clock = new THREE.Clock();
 function render() {
-  controls.update();
+  // controls.update();
+  const deltaTime = clock.getDelta();
+  // cubeGround.rotation.x = time * 0.5;
+  // cubeGround.rotation.y = time * 0.5;
+
+  // sjxGrounp.rotation.x = time * 0.4;
+  // sjxGrounp.rotation.y = time * 0.3;
+
+  // smallBall.position.x = Math.sin(time) * 3;
+  // smallBall.position.z = Math.cos(time) * 3;
+  // smallBall.position.y = 2 + Math.sin(time * 5);
+  // ballGrounp.rotation.z = Math.sin(time) * 0.1;
+  // ballGrounp.rotation.x = Math.sin(time) * 0.1;
+  camera.position.y = -(window.scrollY / window.innerHeight) * 30;
+  camera.position.x += (mouse.x * 10 - camera.position.x) * deltaTime * 5;
+  // camera.position.x += mouse.x * 10 * deltaTime;
+
   renderer.render(scene, camera);
   // 渲染下一帧的时候就会调用render函数
   requestAnimationFrame(render);
 }
+
+let currentPage = 0;
+const groupArr = [cubeGround, sjxGrounp, ballGrounp];
+window.addEventListener('scroll', () => {
+  const newPage = Math.round(window.scrollY / window.innerHeight);
+  if (newPage != currentPage) {
+    currentPage = newPage;
+    console.log('页面更该', currentPage);
+
+    gsap.to(groupArr[currentPage].rotation, {
+      z: `+=${Math.PI * 2}`,
+      x: `+=${Math.PI * 2}`,
+      duration: 2,
+      onComplete() {
+        console.log('旋转完成');
+      },
+    });
+
+    gsap.fromTo(
+      `.page${currentPage + 1} h1`,
+      {
+        x: -300,
+      },
+      {
+        x: 0,
+        rotate: '+=360',
+        duration: 1.5,
+      }
+    );
+  }
+});
 
 render();
